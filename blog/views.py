c0c1d183ead3ecf9f30124d4844bb4a8ba84073c
trojan1771 <76389598+trojan1771@users.noreply.github.com>
2021-10-09 01:35:03 +0530

    My first Django based website
    Learning Django and working on a simple website that stores posts and shows them later .

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,render_to_response, get_object_or_404
+from django.http import HttpResponse
+from .models import Post
+
+
+# Create your views here.
+def index(request):
+    posts=Post.objects.all()
+    return render(request,'index.html',{'posts':posts})
+
+def post(request,slug):
+    print(slug)
+    return render_to_response('post.html',{'post':get_object_or_404(Post,slug=slug)})
+
+def about(request):
+    return render(request,'about.html',{})
